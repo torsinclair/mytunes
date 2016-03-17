8b36155f6b31b5a70cc496e5065583621d2f0fa9
torsinclair <tor@romanesco.se>
2016-03-17 14:46:38 -0700

    Render song queue in dom

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -4,6 +4,6 @@ var SongQueueEntryView = Backbone.View.extend({
   template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
 
   render: function() {
-    return this.$el.html(this.template(this.model.attributes));
+    return this.$el.html(this.template(this.model.attributes)); 
   }
 });
